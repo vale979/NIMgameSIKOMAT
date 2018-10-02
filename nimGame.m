0df2241531eb6e1a5913bc1ad76d5391cf48e962
Valerian Mahdi Pratama <yakira979@gmail.com>
2018-10-02 21:09:00 +0700

    Update nimGame.m

--- nimGame.m
+++ nimGame.m
@@ -1,4 +1,4 @@
-function nimGame()
+
   %inisialisasi vector yang akan dipakai utk converter angka biner
   a = [];
   b = [];
@@ -15,6 +15,26 @@ function nimGame()
     end
   end;
   
+  %narasi awal permainan
+  namaPlayer = input('Huehehee... Selamat datang! Siapa nama anda, wahai penantang?\n');
+  fprintf('Selamat datang, wahai %s!\n',namaPlayer);
+  fprintf('Raja iblis telah kembali. Ia menawan banyak sekali jiwa dalam kegelapan.\n');
+  fprintf('Kau adalah kesatria pemberani yang akan menantang Raja Iblis. Kau adalah harapan terakhir umat manusia..\n');
+  fprintf('Namun, Raja Iblis tidak suka kekerasan. Oleh karena itu kau harus mengalahkannya dalam permainan logika.\n');
+  fprintf('Raja Iblis menantangmu untuk bermain 3-Heap Nim Game.\n');
+  fprintf('Dapatkah kau mengalahkannya untuk menyelamatkan umat manusia dari kegelapan?\n');
+  fprintf('Kita mulai saja permainannya. HAHAHA!\n');
+  
+  fprintf('Peraturan: Siapa yang mengambil batu terakhir yang menang.\n');
+  fprintf('Setiap turn kamu hanya dapat mengambil dari 1 heap.\n');
+  fprintf('Jangan curang, ini bukan CTF.\n');
+  
+  fprintf('Game dimulai!\n');
+  fprintf('HEAP 1: %i\n', heapA);
+  fprintf('HEAP 2: %i\n', heapB);
+  fprintf('HEAP 3: %i\n', heapC);
+  
+  %inti gamenya
   while(endGameCondition(heapA,heapB,heapC) = false)
     %insert the game here
   end
