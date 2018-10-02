1b88e7358a8a864401b46b82b6542035e65b9344
Valerian Mahdi Pratama <yakira979@gmail.com>
2018-10-02 20:52:20 +0700

    Update nimGame.m

--- nimGame.m
+++ nimGame.m
@@ -1,1 +1,21 @@
-
+function nimGame()
+  %inisialisasi vector yang akan dipakai utk converter angka biner
+  a = [];
+  b = [];
+  c = [];
+  verifiedStart = false;
+  while(verifiedStart ~= true)
+    %rig the game so the player wont have winning position at start
+    stoneAmount = randi(maxStone,1,3);
+    heapA = stoneAmount(1);
+    heapB = stoneAmount(2);
+    heapC = stoneAmount(3);
+    if(hitungXOR(heapA, heapB, heapC) == 0)
+      verifiedStart = true;
+    end
+  end;
+  
+  while(endGameCondition(heapA,heapB,heapC) = false)
+    %insert the game here
+  end
+end
