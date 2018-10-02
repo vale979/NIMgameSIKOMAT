28a3cb05686571e597afafe3b1cdbaa54bd7ef3f
Valerian Mahdi Pratama <yakira979@gmail.com>
2018-10-02 21:51:56 +0700

    Update nimGame.m

--- nimGame.m
+++ nimGame.m
@@ -3,9 +3,13 @@ function nimGame()
   a = [];
   b = [];
   c = [];
+  
   verifiedStart = false;
+  
+  maxStone = 1000; %bisa diganti-ganti sesuai keperluan heehi
+  
   while(verifiedStart ~= true)
-    %rig the game so the player wont have winning position at start
+    %rig the game so the player wont have winning position at start huhehe
     stoneAmount = randi(maxStone,1,3);
     if(hitungXOR(stoneAmount(1), stoneAmount(2), stoneAmount(3)) == 0)
       verifiedStart = true;
@@ -15,7 +19,7 @@ function nimGame()
   %narasi awal permainan
   namaPlayer = input('Huehehee... Selamat datang! Siapa nama anda, wahai penantang?\n');
   fprintf('Selamat datang, wahai %s!\n',namaPlayer);
-  fprintf('Raja iblis telah kembali. Ia menawan banyak sekali jiwa dalam kegelapan.\n');
+  fprintf('Raja iblis telah kembali. Ia telah mengurung banyak sekali jiwa dalam kegelapan.\n');
   fprintf('Kau adalah kesatria pemberani yang akan menantang Raja Iblis. Kau adalah harapan terakhir umat manusia..\n');
   fprintf('Namun, Raja Iblis tidak suka kekerasan. Oleh karena itu kau harus mengalahkannya dalam permainan logika.\n');
   fprintf('Raja Iblis menantangmu untuk bermain 3-Heap Nim Game.\n');
@@ -27,34 +31,37 @@ function nimGame()
   fprintf('Jangan curang, ini bukan CTF (-_- ").\n');
   
   fprintf('Game dimulai!\n');
-  fprintf('HEAP 1: %i\n', stoneAmount(1));
-  fprintf('HEAP 2: %i\n', stoneAmount(2));
-  fprintf('HEAP 3: %i\n', stoneAmount(3));
-  
   %ini flip-flop antara 0/1. Kalau 1 berarti giliran player yg jalan. Kalau 0 giliran COM.
   giliranJalan = 1;
 
   %inti gamenya
   while(endGameCondition(stoneAmount(1), stoneAmount(2), stoneAmount(3)) == false)
+    fprintf('Kondisi terkini:\n');
+    fprintf('HEAP 1: %i\n', stoneAmount(1));
+    fprintf('HEAP 2: %i\n', stoneAmount(2));
+    fprintf('HEAP 3: %i\n', stoneAmount(3));
     if(giliranJalan == 1)
+      fprintf('Giliran %s!\n',namaPlayer);
       chooseHeap = input('Mau ambil dari tumpukan yang mana?\n');
       if((chooseHeap > 3) || (chooseHeap < 0))
         %Kalau input ga valid
         fprintf('Inputmu ga valid. Kamu pikir ini CTF?\n');
-        fprintf('Saya ga suka dicurangi begini, kamu kalah.\n');
+        fprintf('Saya ga suka dicurangi begini, kamu kalah!.\n');
         return;
       end
       chooseAmount = input('Oke, mau ambil berapa?');
       if((chooseAmount > stoneAmount(chooseHeap)) || (chooseAmount <= 0))
         %Kalau input ga valid
         fprintf('Inputmu ga valid. Kamu pikir ini CTF?\n');
-        fprintf('Saya ga suka dicurangi begini, kamu kalah.\n');
+        fprintf('Saya ga suka dicurangi begini, kamu kalah!.\n');
         return;
       end
       fprintf('Oke, kamu ambil %i batu dari heap ke-%i\nJangan nyesal ya..',chooseAmount,chooseHeap);
+      stoneAmount(chooseHeap) = stoneAmount(chooseHeap)-chooseAmount;
       giliranJalan = 0;
     else
       %insert COM routine here
+      fprintf('Giliran Raja Iblis!\n');
     end
   end
 end
