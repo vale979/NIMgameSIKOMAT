a0b8cbb04ed4a1a056b5ae9869644b9f69924084
Valerian Mahdi Pratama <yakira979@gmail.com>
2018-10-02 23:05:09 +0700

    Update stepCOM.m

--- stepCOM.m
+++ stepCOM.m
@@ -1,3 +1,39 @@
 function [source, amount] = stepCOM(numA, numB, numC)
-%insert function here
+%inisialisasi
+source = -1;
+amount = -1;
+ketemu = false;  
+%BRUTE FORCE FUEHEHE
+%mulai dari heap pertama dulu
+  for i = stoneAmount(1):-1:1
+    if(hitungXOR(stoneAmount(1) - i, stoneAmount(2), stoneAmount(3)) == 0)
+      source = 1;
+      amount = i;
+      ketemu = true;
+      return;
+    end
+  end
+  if(ketemu == false)
+    %coba heap kedua
+    for i = stoneAmount(2):-1:1
+      if(hitungXOR(stoneAmount(1), stoneAmount(2)-i, stoneAmount(3)) == 0)
+        source = 2;
+        amount = i;
+        ketemu = true;
+      end
+    end
+    if(ketemu == false)
+      %kalau masih belum ketemu juga
+      for i = stoneAmount(3):-1:1
+        if(hitungXOR(stoneAmount(1), stoneAmount(2), stoneAmount(3) - i) == 0)
+          source = 3;
+          amount = i;
+          ketemu = true;
+        end
+      end
+    end
+  end
+  if(source == -1)
+    error('Something wrong...');
+  end
 end
