ad138f8d9a480f7df021b8c332da3ea3d821bfcf
salmanhiro <salman.consulting@outlook.com>
2018-10-04 00:06:57 +0700

    Update convertToBinary.m
    ada 3 jadi vekbin1, vekbin2, vekbin3

--- convertToBinary.m
+++ convertToBinary.m
@@ -1,30 +1,64 @@
-function convertToBinary(num) %hey there, we have 3 num to be converted into array of binary!
+function convertToBinary(num1,num2,num3) %hey there, we have 3 num to be converted into array of binary!
+  %insert function here
+  
+num1 = input('enter num1: '); % kalo ada di main, abaikan 
+num2 = input('enter num2: ');
+num3 = input('enter num3: ');
 
-num = input('enter decimal: '); % kalo ada di main, abaikan 
 
 i = 1; % jadi awalnya ada i untuk indeks vektor "biner"
-reminder = rem(num, 2); % bilangan ini kita bagi 2 terus dicari remindernya
-biner(i) = num2str(reminder(i)); %kita isi vektor "biner" ini dengan remindernya
+j = 1;
+k = 1;
 
-flooring = floor(num/2);
-if num <= 31
-  while flooring >= 2
-    num = flooring;
-    flooring = floor(num/2);
-    remind = rem(num, 2);
-    i = i + 1;
-    biner(i) = num2str(remind);
-  end
-else
-  fprintf('your input must in range of 1-31\n')
+reminder1 = rem(num1, 2);
+reminder2 = rem(num2, 2);
+reminder3 = rem(num3, 2);% bilangan ini kita bagi 2 terus dicari remindernya
+
+biner1(i) = num2str(reminder1(i)); %kita isi vektor "biner" ini dengan remindernya
+biner2(j) = num2str(reminder3(j));
+biner3(k) = num2str(reminder3(k));
+
+flooring1 = floor(num1/2);
+flooring2 = floor(num2/2);
+flooring3 = floor(num3/2);
+
+while flooring1 >= 2
+  num1 = flooring1;
+  flooring1 = floor(num1/2);
+  reminder1 = rem(num1, 2);
+  i = i + 1;
+  biner1(i) = num2str(reminder1);
+end
+biner1(i + 1) = num2str(flooring1);
+vekbin1 = fliplr(biner1);
+
+while flooring2 >= 2
+  num2 = flooring2;
+  flooring2 = floor(num2/2);
+  reminder2 = rem(num2, 2);
+  j = j + 1;
+  biner2(j) = num2str(reminder2);
+end
+biner2(j + 1) = num2str(flooring2);
+vekbin2 = fliplr(biner2);
+
+while flooring3 >= 2
+  num3 = flooring3;
+  flooring3 = floor(num3/2);
+  reminder3 = rem(num3, 2);
+  k = k + 1;
+ biner3(k) = num2str(reminder3);
+end
+biner3(k + 1) = num2str(flooring3);
+vekbin3 = fliplr(biner3); % ini vektornya
+
+disp(vekbin1)
+disp(vekbin2)
+disp(vekbin3)
+
 end
 
-biner(i + 1) = num2str(flooring);
-biner = fliplr(biner);
 
-vekbin = biner-'0'; % ini vektornya
-
-end
 
 % misal 12
 % bagi 2, reminder = 0
@@ -48,4 +82,3 @@ end
 % kita balik soalnya dia ngisi dari depan, sedangkan biner bacanya dari belakang, jadi 1100
 % dipecah digitnya jadi 1 1 0 0 (itu cara mecahnya pake yang -'0')
 % dapet representasi biner 12
-
