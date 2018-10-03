104f24a74aab5f363a1cfd00bc776f86421c1ce4
izzako <43785121+izzako@users.noreply.github.com>
2018-10-03 09:08:03 +0700

    update hitung XOR
    ini akun github izzan btw

--- hitungXOR.m
+++ hitungXOR.m
@@ -1,3 +1,10 @@
 function res = hitungXOR(numA, numB, numC)
   %insert function here
+  %insert pemanggilan fungsi convertbiner here
+  %ubah biner ke vektor biner here
+  
+  %note: vektor biner dinamakan vekbinA, vekbinB, dan vekbinC
+  
+  sumVekbin = vekbinA + vekbinB + vekbinC;
+  jumlahXOR = mod(sumVekbin,2);
 end
