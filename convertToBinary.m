d4efdc0d0256cfce79db3199b4fbcc5d63084021
salmanhiro <salman.consulting@outlook.com>
2018-10-03 19:28:53 +0700

    Update convertToBinary.m

--- convertToBinary.m
+++ convertToBinary.m
@@ -1,3 +1,28 @@
 function convertToBinary(num)
   %insert function here
+num = input('enter decimal: '); % kalo ada di main, abaikan 
+
+i = 1;
+remind = rem(num, 2);
+biner(i) = num2str(remind(i));
+
+flooring = floor(num/2);
+if num <= 31
+  while flooring >= 2
+    num = flooring;
+    flooring = floor(num/2);
+    remind = rem(num, 2);
+    i = i + 1;
+    biner(i) = num2str(remind);
+  end
+else
+    fprintf('your input must in range of 1-31\n')
+    break
+end
+
+biner(i + 1) = num2str(flooring);
+biner = fliplr(biner);
+
+binaryvector = biner-'0'; % ini vektornya
+
 end
